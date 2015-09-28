f9b5c9bbee186df398a059f4305ae06adbd44e25
jonathan <infixz520@gmail.com>
2015-09-28 19:21:14 +0800

    resume demo

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 # coding:utf-8
+import os.path
 
 import tornado.httpserver as httpserver
 import tornado.ioloop as ioloop
@@ -11,11 +12,29 @@ class IndexHandler(web.RequestHandler):
 	#	self.arg = arg
 	def get(self):
 		greetingwords = self.get_argument('greeting','hello')
-		self.write(greetingwords + ',see you again now')
+		self.write(greetingwords + u',再次见面分外眼红')
+	def write_error(self,status_code,**Kwargs):
+		self.write("日了狗，你把我服务器弄坏了！")
+
+
+class ResumeHandler(web.RequestHandler):
+	"""docstring for ResumeHandler"""
+	#def __init__(self, arg):
+	#	super(ResumeHandler, self).__init__()
+	#	self.arg = arg
+	def get(self):
+		self.render('resume.html')
+	def write_error(self,status_code,**Kwargs):
+		self.write("日了狗，你把我服务器弄坏了！")
 
 
 if __name__ == '__main__':
-	app = web.Application(handlers=[(r'/',IndexHandler)])
+	app = web.Application(
+		handlers=[(r'/',IndexHandler),(r'/resume',ResumeHandler)],
+		template_path=os.path.join(os.path.dirname(__file__),"templates"),
+		static_path=os.path.join(os.path.dirname(__file__), "static"),
+		debug=True
+		)
 	http_server = httpserver.HTTPServer(app)
-	http_server.listen(80)
+	http_server.listen(8080)
 	ioloop.IOLoop.instance().start()
