1adeb0a3583874d6638a7bf714844bc0e1c1392c
jonathan <infixz520@gmail.com>
2015-09-29 21:51:01 +0800

    app class

--- server.py
+++ server.py
@@ -5,6 +5,9 @@ import tornado.httpserver as httpserver
 import tornado.ioloop as ioloop
 import tornado.web as web
 
+from pymongo import MongoClient
+
+
 class IndexHandler(web.RequestHandler):
 	"""docstring for IndexHandler"""
 	#def __init__(self, arg):
@@ -13,11 +16,6 @@ class IndexHandler(web.RequestHandler):
 	def get(self):
 		greetingwords = self.get_argument('greeting','hello')
 		self.write(greetingwords + u',再次见面分外眼红')
-	def write_error(self,status_code,**Kwargs):
-		resp = '' 
-		if status_code == 404:
-			resp = '404只'
-		self.write("<h1>日了%s狗，你把我服务器弄坏了！</h1>"% resp)
 
 
 class ResumeHandler(web.RequestHandler):
@@ -34,13 +32,26 @@ class ResumeHandler(web.RequestHandler):
 		self.write("<h1>日了%s狗，你把我服务器弄坏了！</h1>"% resp)
 
 
+class App(web.Application):
+	"""docstring for App"""
+	def __init__(self, arg):
+		handlers = [(r'/',IndexHandler),(r'/resume',ResumeHandler)]
+		template_path = os.path.join(os.path.dirname(__file__),"templates")
+		static_path = os.path.join(os.path.dirname(__file__), "static")
+		client = MongoClient()
+		self.db = client.usertest
+		web.Application.__init__(self,handlers,template_path,static_path,debug=True)
+
+
+
+
 if __name__ == '__main__':
-	app = web.Application(
+	"""app = web.Application(
 		handlers=[(r'/',IndexHandler),(r'/resume',ResumeHandler)],
 		template_path=os.path.join(os.path.dirname(__file__),"templates"),
 		static_path=os.path.join(os.path.dirname(__file__), "static"),
 		debug=True
-		)
-	http_server = httpserver.HTTPServer(app)
+		)"""
+	http_server = httpserver.HTTPServer(App())
 	http_server.listen(8080)
 	ioloop.IOLoop.instance().start()
